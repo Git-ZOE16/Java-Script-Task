eaf3e9d5b75f6a5759b694fb7ba7a8f6322e9251
Zoe <zoeinspired1609@gmail.com>
2026-05-07 09:51:47 +0100

    sum and division of three numbers

---
+++ EasterEggs/MeatPie/sumdivision.js
@@ -0,0 +1,7 @@
+let prompt = require("prompt-sync")();
+
+let number1 = Number(prompt("Enter First number"));
+let number2 = Number(prompt("Enter Second number"));
+let number3 = Number(prompt("Enter Third number"));
+
+console.log((number1 + number2 + number3)/3);
